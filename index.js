ed6157611e61eca30f1b4f7cf9efa5cf17f11470
Rene Weber <weber.rene@live.de>
2017-01-02 18:20:33 +0100

    Define js interface

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 
 import { NativeModules } from 'react-native';
 
-const { RnRecord } = NativeModules;
+const { RnRecord as NativeRnRecord } = NativeModules;
 
-export default RnRecord;
+export default class RnRecord {
+    id: Number;
+
+    save(): Number {
+        return NativeRnRecord.save(this.getProperties());
+    }
+
+    update(): Boolean {
+        return NativeRnRecord.update(this.getProperties());
+    }
+
+    remove(): Boolean {
+        return NativeRnRecord.remove(this.getProperties());
+    }
+
+    static findAll(): Array {
+        return NativeRnRecord.findAll(this.name).map(rec => this.transformResultObj(this.name, rec));
+    }
+
+    static find(query: Object): Array {
+        return NativeRnRecord.find(this.name, query).map(rec => this.transformResultObj(this.name, rec));
+    }
+
+    private transformResultObj(className: String, obj: Object) {
+        const inst = new className;
+        Object.getOwnPropertyNames(obj).forEach(k => {
+            inst[k] = obj[k];
+        });
+
+        return inst;
+    }
+
+    private getProperties() {
+        const propNames = Object.getOwnPropertyNames(this);
+        return propNames.reduce( (obj, name) => obj[name] = this[name], {});
+    }
+}
